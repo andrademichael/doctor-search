328a2f145dc14a816ff7e3393fda6a78df32717f
michael <andrade.michael.e@gmail.com>
2017-01-06 11:42:41 -0800

    initialize Bower

--- js/doctorsearch.js
+++ js/doctorsearch.js
@@ -1,14 +1,11 @@
-function Patient(name, zip) {
-  this.patientName = name;
-  this.zipCode = zip;
-  this.searches = [];
-}
+exports.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
 
-Patient.prototype.searchDoctors = function(condition) {
-  var output = [];
-  while (moreDocs) {
-    $.get("")
-  }
-}
-
-exports.doctorSearchModule = Patient
+exports.doctorSearchModule = getDoctors
