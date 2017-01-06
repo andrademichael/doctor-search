8207ac4a7dc1c843e65a126e138cf69720ab3c55
michael <andrade.michael.e@gmail.com>
2017-01-06 15:38:18 -0800

    add backend logic to make API call and retrieve first_name, last_name, and specialties from the JSON response

--- js/doctorSearch.js
+++ js/doctorSearch.js
@@ -1,10 +1,13 @@
-exports.getDoctors = function(medicalIssue) {
+exports.doctorSearch = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
     var foundDoctors = [];
+    firstName = response.data.profile.first_name;
+    lastName = response.data.profile.last_name;
+    specialties = response.data.specialities;
+
     console.log("API call returned: " + result);
-    parsedResult = JSON.parse(result);
-    for (i=0; i < parsedResult.length; i++)
+    for (i=0; i < .length; i++)
       doctorArray.push(result[i]);
     return foundDoctors;
   })
@@ -14,6 +17,3 @@ exports.getDoctors = function(medicalIssue) {
 };
 
 doctorSearchResult = getDoctors(condition);
-
-
-exports.doctorSearchModule = doctorSearch;
