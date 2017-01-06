349e12ad1241ed991ac7b3e69c289d4c10d8a8ff
michael <andrade.michael.e@gmail.com>
2017-01-06 14:31:38 -0800

    add Jquery logic for form input and calling doctorSearch method, start writing doctorSearch method

--- js/doctorSearch-interface.js
+++ js/doctorSearch-interface.js
@@ -1,5 +1,12 @@
-const Patient = require('./../js/doctorSearch.js').doctorSearchModule;
+const doctorSearch = require('./../js/doctorSearch.js').doctorSearchModule;
 
 $(function() {
-
+  $("searchForm").submit(function(event) {
+    event.preventDefault;
+    condition = $("#conditionInput").val();
+    foundDoctors = doctorSearch(condition);
+    for (var i = 0; i < localDoctors.length; i++) {
+      $("#doctorDisplay").push("<li>" + foundDoctors[i] + "</li>")
+    }
+  });
 });
