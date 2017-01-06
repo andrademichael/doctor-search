190f9e59ed25cca33931c0baec758bb7007297e1
michael <andrade.michael.e@gmail.com>
2017-01-06 13:53:06 -0800

    install more packages and require them in gulpfile, refactor links in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var utilities = require('gulp-util');
-var del = require('del');
-var jshint = require('gulp-jshint');
-var buildProduction = utilities.env.production;
+var browserSync = require('browser-sync').create();
+var vinylSourceStream = require('vinyl-source-stream');
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
+//Bower Tasks
 
 gulp.task('bowerJS', function () {
   return gulp.src(lib.ext('js').files)
@@ -14,6 +22,16 @@ gulp.task('bowerJS', function () {
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
 });
+
+gulp.task("bowerCSS", function() {
+  return gulp.src(lib.ext("css").files)
+    .pipe(concat("vendor.css"))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+//Gulp Tasks
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -26,4 +44,26 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('build', ['clean'], function() {
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+  gulp.start('bower');
+});
+
+gulp.task('clean'), function() {
+
+}
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
 });
