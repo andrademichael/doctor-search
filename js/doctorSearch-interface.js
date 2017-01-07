9b341fb598ec7838e1cc33dec398022fb763198e
michael <andrade.michael.e@gmail.com>
2017-01-06 16:47:23 -0800

    hunt bugs in export module js files

--- js/doctorSearch-interface.js
+++ js/doctorSearch-interface.js
@@ -1,10 +1,12 @@
+var apiKey = require('./../.env').apiKey;
+
 var doctorSearch = require('./../js/doctorSearch.js').doctorSearchModule;
 
 $(function() {
-  $("searchForm").submit(function(event) {
+  $("#searchForm").submit(function(event) {
     event.preventDefault();
-    condition = $("#conditionInput").val();
-    displayDoctors = doctorSearch(condition);
+    conditionType = $("#conditionInput").val();
+    displayDoctors = doctorSearch(conditionType);
     for (var i = 0; i < localDoctors.length; i++) {
       $("#doctorDisplay").push("<li>" + foundDoctors[i] + "</li>");
     }
--- js/doctorSearch.js
+++ js/doctorSearch.js
@@ -1,9 +1,11 @@
+var apiKey = require('./../.env').apiKey;
+
 //getDoctors method returns an array of doctor objects
 exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&skip=0&limit=20&specialty_uid=' + medicalIssue + '&user_key=' + apiKey)
   //success branch
   .then(function(result) {
-    var parsedResult = JSON.parse(result)
+    var parsedResult = JSON.parse(result);
     console.log("API call returned: " + result + ", which was then parsed into: " + parsedResult);
     //populate doctors array by looping through result's data objects
     var doctors = [];
@@ -17,16 +19,3 @@ exports.getDoctors = function(medicalIssue) {
     console.log("fail");
   });
 };
-
-    //outer loop goes through every returned doctor object
-    result.data.forEach(function(doctor) {
-      //inner loop goes through that doctor's specialties looking for match to the search term
-      doctor.specialties.forEach(function(specialty) {
-        if
-      });
-      if ()
-      foundDoctors.push(key + ": " + data.obj[key])
-    });
-
-
-doctorSearchResult = doctorSearch(condition);
